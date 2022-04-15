52b7ec96568e7255f3449eb60ad191f152d6a19c
Shuhei Yoshida <aneki525@gmail.com>
2022-04-15 10:58:25 +0900

    first

---
+++ SwiftMVVM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftMVVM
+//
+//  Created by 吉田周平 on 2022/04/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
